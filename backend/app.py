9a8df13909bdb77852ae1effbb944c32f5f31329
David Omrai <david.omrai@seznam.cz>
2021-11-23 18:55:01 +0100

    Figurated out the comunication

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,29 @@ from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__)
 
+data = {
+    "name": "Dave",
+    "rtl": 'there is none'
+}
+
 @app.route('/api', methods=['GET'])
 def index():
-    return {
-        "name": "Dave",
-        "rtl": 'there is none'
-    }
+    return data
+
+@app.route('/add', methods=['POST'], strict_slashes=False)
+def add_something():
+    print(request)
+    #name = request['name']
+    #body = request['rtl']
+    global data
+    data = request.get_json()
+    # data = {
+    #     'name': 'poop',
+    #     'rtl': 'peep',
+    # }
+
+    return data
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') 
--- frontend/twimetaking/src/App.jsx
+++ frontend/twimetaking/src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+
+import { Button } from '@material-ui/core';
 import './App.css';
 
 import TestPage from './components/TestPage';
@@ -15,6 +17,30 @@ function App() {
   return (
     <div className="App">
       <TestPage data={initialState} />
+      <Button
+        onClick={async () => {
+          const newData = {
+            name: 'Dave Liborius',
+            rtl: 'Im not quite sure',
+          };
+
+          const response = await fetch('/add', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newData),
+          });
+          if (response.ok) {
+            console.log('response worked');
+            fetch('/api').then((r) => r.json().then((data) => {
+              setState(data);
+            }));
+          }
+        }}
+      >
+        submit
+      </Button>
     </div>
   );
 }
