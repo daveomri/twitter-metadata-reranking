c44d6f6dadd82d93d596810f5f0943007f2b02f5
David Omrai <david.omrai@seznam.cz>
2021-12-04 14:10:22 +0100

    Changed the spaces from 4 to 2

--- backend/Tweets.py
+++ backend/Tweets.py
@@ -3,24 +3,18 @@ import pandas as pd
 from components.TwitterAPI import TwitterAPI
 
 class Tweets:
-    def __init__(self):
-        # Twitter API
-        self.twiapi = TwitterAPI()
-        # Tweets
-        self._tweets = pd.DataFrame(self.twiapi.tweet_features)
-    
-    def fetch_tweets(self, hashtags, usernames, length):
-        usernames_tweets = self.twiapi.get_tweets_by_usernames(usernames, length)
-        hashtags_tweets = self.twiapi.get_tweets_by_hashtags(hashtags, length)
+  def __init__(self):
+    # Twitter API
+    self.twiapi = TwitterAPI()
+    # Tweets
+    self._tweets = pd.DataFrame(self.twiapi.tweet_features)
+      
+  def fetch_tweets(self, hashtags, usernames, length):
+    usernames_tweets = self.twiapi.get_tweets_by_usernames(usernames, length)
+    hashtags_tweets = self.twiapi.get_tweets_by_hashtags(hashtags, length)
 
-        self._tweets = pd.concat([usernames_tweets, hashtags_tweets], ignore_index=True)
+    self._tweets = pd.concat([usernames_tweets, hashtags_tweets], ignore_index=True)
 
 
-    def sort_tweets(self, sort_params):
-        """todo"""
-
-tweets = Tweets()
-
-tweets.fetch_tweets(['death'], ['DaveOmri'], 2)
-
-print(tweets._tweets.id)
+  def sort_tweets(self, sort_params):
+    """todo"""
